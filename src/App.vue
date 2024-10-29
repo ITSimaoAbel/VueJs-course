5006ff988ba814f453ba07de262b5f2001cea3d8
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 13:09:31 +0200

    Update: create component Exemplo04 and  Diretivas v-html

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
   //importacao dos componentes
   //import Exemplo01 from './components/Exemplo01Interpolacao.vue';
   //import Exemplo02 from './components/Exemplo02Condicional.vue';
-  import Exemplo03 from './components/Exemplo03Laco.vue';
+  //import Exemplo03 from './components/Exemplo03Laco.vue';
+  import Exemplo04 from './components/Exemplo04HTML.vue';
 
 </script>
    
 <!-- HTML -->
  <template>
-    <Exemplo03></Exemplo03>
+    <Exemplo04></Exemplo04>
  </template>
---
+++ src/components/Exemplo04HTML.vue
@@ -0,0 +1,13 @@
+<!-- SCRIPT -->
+ <script setup> 
+    // Variavel
+    let texto = '<p style="color:red">A prendendo a directiva v-html</p>';
+
+</script>
+
+<!-- SCRIPT -->
+ <template>
+    <div v-html="texto"></div>
+
+    <div>{{ texto }}</div>
+ </template>
