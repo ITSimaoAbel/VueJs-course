7bafa2ef6a4a6a70c5ae438cab57745dc269fbf1
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 13:21:29 +0200

    Update: create component Exemplo05 and  Diretivas v-model

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@
   //import Exemplo01 from './components/Exemplo01Interpolacao.vue';
   //import Exemplo02 from './components/Exemplo02Condicional.vue';
   //import Exemplo03 from './components/Exemplo03Laco.vue';
-  import Exemplo04 from './components/Exemplo04HTML.vue';
+  //import Exemplo04 from './components/Exemplo04HTML.vue';
+  import Exemplo05 from './components/Exemplo05Model.vue';
 
 </script>
    
 <!-- HTML -->
  <template>
-    <Exemplo04></Exemplo04>
+    <Exemplo05></Exemplo05>
  </template>
