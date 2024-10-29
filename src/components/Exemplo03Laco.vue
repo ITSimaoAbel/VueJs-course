3f414754273e0a6d17807e6e7549a94a80a5e177
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 12:56:27 +0200

    Update: create component Exemplo03 and  Diretivas v-for

---
+++ src/components/Exemplo03Laco.vue
@@ -0,0 +1,12 @@
+<!--SCRIPT-->
+<script setup>
+    //Vetor
+    let nomes = ['Alice', 'Joao', 'Maria', 'Jose', 'Pedro'];
+</script>
+
+<!-- HTML -->
+<template>
+    <ul>
+        <li v-for="nome in nomes">{{ nome }}</li>
+    </ul>
+</template>
