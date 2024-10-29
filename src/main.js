cffd46aaf01f6d6386207575fbdc97b6f97768bc
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 11:36:38 +0200

    Interpolacao de texto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
+//<odulo create App (para criar a nossa aplicacao)
 import { createApp } from 'vue'
+
+//Importar arquivo App.vue (raiz da aplicacao)
 import App from './App.vue'
 
+//render
 createApp(App).mount('#app')
