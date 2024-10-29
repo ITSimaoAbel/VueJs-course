0d67f23b1647f4c1a9860be2ae4e7964f1f38362
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 15:04:06 +0200

    Create evento de submissao

---
+++ src/components/Exemplo09Enviar.vue
@@ -0,0 +1,16 @@
+<!-- SCRIPT -->
+<script setup>
+
+    //Funcao para validar formulario
+    function validarFormulario(){
+        alert("formulário enviado com sucesso!");
+    }
+
+</script>
+
+<!-- HTML -->
+ <template>
+    <form @submit="validarFormulario">
+        <input type="submit" value="Enviar formulario">
+    </form>
+ </template>
