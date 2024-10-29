ca6801341896cb901c576b95f13b66ea5d338b53
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 13:43:52 +0200

    Update: create component Exemplo06 and  interligacao de classes

---
+++ src/components/Exemplo06Classe.vue
@@ -0,0 +1,18 @@
+<!-- SCRIPT -->
+<script setup>
+    //variavel
+    let validacao = true;
+
+</script>
+
+<!-- CSS -->
+<style>
+    .ok{color:green}
+
+    .falha{color:red}
+</style>
+
+<!-- HTML -->
+ <template>
+    <p :class="validacao ? 'ok' : 'falha'">Utilizando a directiva class</p>
+ </template>
