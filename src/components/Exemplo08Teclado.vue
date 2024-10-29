e4d4b6d44dd5bb6dcf99ec81477f994bf69d0e7e
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 14:48:42 +0200

    Update crea Exemplo08 and enventos de teclado

---
+++ src/components/Exemplo08Teclado.vue
@@ -0,0 +1,13 @@
+<!-- SCRIPT -->
+<script setup>
+    //Funcao de mensagem
+    function mensagem() {
+        alert("Executando a funcao mensagem");
+    }
+</script>
+
+
+<!-- HTML -->
+ <template>
+    <input type="text" @keyup.enter="mensagem">
+ </template>
