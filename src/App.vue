888c5f6df0b2fd3b2e60035f80eee71215f8c1a5
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 12:32:39 +0200

    Update: create Diretiva v-if

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <!-- SCRIPT -->
 <script setup>
   //importacao dos componentes
-  import Exemplo01 from './components/Exemplo01Interpolacao.vue';
+  //import Exemplo01 from './components/Exemplo01Interpolacao.vue';
+  import Exemplo02 from './components/Exemplo02Condicional.vue';
 </script>
    
 <!-- HTML -->
  <template>
-    <Exemplo01></Exemplo01>
+    <Exemplo02></Exemplo02>
  </template>
---
+++ src/components/Exemplo02Condicional.vue
@@ -0,0 +1,10 @@
+<!-- SCRIPT -->Exemplo02Condicional.vue
+<script setup>
+    // Variavel
+    let exibir = true;
+</script>
+
+<!-- HTML -->
+<template>
+    <h1 v-if="exibir">Hello World!</h1>
+</template>
