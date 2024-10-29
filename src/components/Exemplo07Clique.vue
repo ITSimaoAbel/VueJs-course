6c7cf8ed25ce7f618a8ef35c0746604ead888312
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-29 14:36:42 +0200

    Update crea Exemplo07 and

---
+++ src/components/Exemplo07Clique.vue
@@ -0,0 +1,18 @@
+<!-- SCRIPT -->
+ <script setup>
+
+    //>Funcao
+    function mensagem() {
+        alert('Trabalhando com eventos de clique');
+    }
+
+</script>
+
+
+<!-- HTML -->
+ <template>
+
+   <!-- <h1>Trabalhando com eventos de clique</h1> -->
+    <button @click="mensagem()">Clique aqui</button>
+
+ </template>
