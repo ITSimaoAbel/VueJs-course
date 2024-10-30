27bb6c5da3965707e3f51468d30976a7272b1d15
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-30 11:41:49 +0200

    Update: Create Props dinamicos

--- src/components/Exemplo11Props1.vue
+++ src/components/Exemplo11Props1.vue
@@ -3,9 +3,18 @@
 
     //Importar componente 
     import Exemplo11Props2 from './Exemplo11Props2.vue';
+
+    //variaveis
+    let cidade = 'Maputo';
+    let cidades = ['Matola', 'Maputo', 'Chimoio', 'Xai-Xai'];
+
+    //funcao
+    function mensagem() {
+        alert('Funcao criada no componente: Exemplo11Props1');
+    }
 </script>
 
 <!-- HTML -->
  <template>
-    <Exemplo11Props2 nome="Simao" idade="25"></Exemplo11Props2>
+    <Exemplo11Props2 nome="Simao" idade="25" :cidade="cidade" :vetor="cidades" :funcao="mensagem"></Exemplo11Props2>
  </template>
